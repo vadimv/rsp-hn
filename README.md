7094f8a7dd9e92ea5db1ca911d4d2a0952e4c5e9
Vadim Vashkevich <vadim.vashkevich@gmail.com>
2021-02-06 21:50:44 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# rsp-hn
+# RSP Hacker News Client
+
+A Hacker News API client in Java.
+
+## Usage
+
+This project requires Java 11 or newer.
+
+```shell script
+$ mvn clean package
+
+$ java -jar ./target/rsp-hn-0.1-SNAPSHOT-jar-with-dependencies.jar
+```
+
+Navigate to http://localhost:8080.
---
+++ src/main/java/rsp/hn/State.java
@@ -0,0 +1,25 @@
+package rsp.hn;
+
+public class State {
+
+    public final int[] storiesIds;
+    public final Story[] stories;
+    public final int pageNum;
+    public State(int[] storiesIds, Story[] stories, int pageNum) {
+        this.storiesIds = storiesIds;
+        this.stories = stories;
+        this.pageNum = pageNum;
+    }
+
+    public static class Story {
+        public final long id;
+        public final String name;
+        public final String url;
+
+        public Story(long id, String name, String url) {
+            this.id = id;
+            this.name = name;
+            this.url = url;
+        }
+    }
+}
