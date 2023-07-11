ccf6b156197c157feec892c7e45a2883c4e67a28
Vadim Vashkevich <vadim.vashkevich@gmail.com>
2023-07-12 01:46:20 +0200

    Upgrade for rsp 1.0

--- src/main/java/rsp/hn/JettyHn.java
+++ src/main/java/rsp/hn/JettyHn.java
@@ -1,9 +1,10 @@
 package rsp.hn;
 
 import rsp.App;
-import rsp.Component;
 import rsp.jetty.JettyServer;
+import rsp.routing.Routing;
 import rsp.server.StaticResources;
+import rsp.stateview.ComponentView;
 import rsp.util.StreamUtils;
 
 import java.io.File;
@@ -13,7 +14,8 @@ import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 import static java.lang.Float.parseFloat;
-import static rsp.dsl.Html.*;
+import static rsp.html.HtmlDsl.*;
+import static rsp.routing.RoutingDsl.get;
 import static rsp.util.ArrayUtils.concat;
 
 /**
@@ -23,14 +25,14 @@ public class JettyHn {
 
     public static void main(String[] args) throws Exception {
         final HnApiService hnApi = new HnApiService();
-        final var bodyRef = createRef();
-        final Component<State> render = useState ->
+        final var bodyRef = createElementRef();
+        final ComponentView<State> render = state -> newState ->
             html(head(link(attr("rel", "stylesheet"), attr("href","/res/style.css"))),
                     body(bodyRef,
                         div(attr("class", "header"),
                             h3(text("Hacker News"))),
                         div(attr("class", "content"),
-                            of(StreamUtils.zipWithIndex(Arrays.stream(useState.get().stories)).map(story ->
+                            of(StreamUtils.zipWithIndex(Arrays.stream(state.stories)).map(story ->
                                     div(
                                             span((story.getKey() + 1) + ". "),
                                             a(attr("href", story.getValue().url), text(story.getValue().name))
@@ -44,32 +46,33 @@ public class JettyHn {
                                        .thenCompose(pageYOffset -> bodyProps.getString("offsetHeight")
                                        .thenAccept(offsetHeight -> {
                                            if ((parseFloat(innerHeight) + parseFloat(pageYOffset)) >= parseFloat(offsetHeight)) {
-                                               final State currentState = useState.get();
-                                               final int newPageNum = currentState.pageNum + 1;
-                                               final List<Integer> newStoriesIds = pageIds(Arrays.stream(currentState.storiesIds).boxed().collect(Collectors.toList()),
+                                               //final State currentState = state;
+                                               final int newPageNum = state.pageNum + 1;
+                                               final List<Integer> newStoriesIds = pageIds(Arrays.stream(state.storiesIds).boxed().collect(Collectors.toList()),
                                                                                            newPageNum,
                                                                                            HnApiService.PAGE_SIZE);
-                                               final CompletableFuture<State> newState = hnApi.stories(newStoriesIds)
+                                               final CompletableFuture<State> newStateCf = hnApi.stories(newStoriesIds)
                                                                                               .thenApply(newStories ->
-                                                                                                      new State(currentState.storiesIds,
-                                                                                                                concat(currentState.stories,
+                                                                                                      new State(state.storiesIds,
+                                                                                                                concat(state.stories,
                                                                                                                        newStories.toArray(State.Story[]::new)),
                                                                                                                 newPageNum));
-                                               useState.accept(newState);
+                                               newState.applyWhenComplete(newStateCf);
                                            }
                                        })));
                         }).debounce(500)
                     )
                 );
 
-        final var server = new JettyServer(8080,"", new App<State>(
-                                                    request -> hnApi.storiesIds()
-                                                                    .thenCompose(ids -> hnApi.stories(pageIds(ids, 0, HnApiService.PAGE_SIZE))
-                                                                                        .thenApply(r -> new State(ids.stream().mapToInt(Integer::intValue).toArray(),
-                                                                                                              r.toArray(State.Story[]::new),
-                                                                                                             0))),
-                                                    render),
-                                                    new StaticResources(new File("src/main/java/rsp/hn"),
+        final var route =  hnApi.storiesIds()
+                .thenCompose(ids -> hnApi.stories(pageIds(ids, 0, HnApiService.PAGE_SIZE))
+                        .thenApply(r -> new State(ids.stream().mapToInt(Integer::intValue).toArray(),
+                                                    r.toArray(State.Story[]::new),
+                                                    0)));
+
+        final var server = new JettyServer<>(8080,"", new App<>(new Routing<>(get("", req -> route)),
+                                                                             render),
+                                                    new StaticResources(new File("src/main/java/rsp/examples/hn"),
                                                                    "/res/*"));
         server.start();
         server.join();
